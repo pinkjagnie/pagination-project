0963348c29c521e8ffeda0036db8df90541f2079
pinkjagnie <olga.krzysztofik@gmail.com>
2024-01-15 23:44:59 +0100

    Add simple menu-link in home page

--- src/app/page.js
+++ src/app/page.js
@@ -1,87 +1,20 @@
 "use client";
 
-import { useEffect } from "react";
-
-import { useInfiniteQuery, useQueryClient } from "@tanstack/react-query";
+import Link from "next/link";
 
 import ListOfPosts from "./components/ListOfPosts";
-import SinglePost from "./components/SinglePost";
 
 export default function Home() {
-  const queryClient = useQueryClient();
-
-  const getPosts = async (page = 1) => {
-    try {
-      const response = await fetch("/api/posts?page=" + page, {
-        headers: {
-          "Content-Type": "application/json",
-        },
-      });
-
-      if (!response.ok) {
-        throw new Error(`HTTP error! Status: ${response.status}`);
-      }
-
-      const res = await response.json();
-      // console.log(res[3].id);
-      return res;
-    } catch (error) {
-      console.error("Error fetching data:", error);
-    }
-  };
-
-  const { data, fetchNextPage, isFetchingNextPage } = useInfiniteQuery({
-    queryKey: ["query"],
-    queryFn: ({ pageParam }) => getPosts(pageParam),
-
-    getNextPageParam: (lastPage, pages) => {
-      return pages.length + 1;
-      // return lastPage.length > 0 ? pages.length + 1 : undefined;
-    },
-    initialData: { pages: [], pageParams: [1] },
-  });
-
-  useEffect(() => {
-    // Fetch initial data only once
-    if (data.pages.length === 0) {
-      getPosts(1);
-    }
-  }, []);
-
   return (
     <main className="min-h-screen bg-slate-50 pb-10">
       <ListOfPosts />
-      {data.pages &&
-        data.pages.map((page, i) => {
-          return (
-            <div key={i}>
-              {page &&
-                page.map((post) => {
-                  return (
-                    <SinglePost
-                      key={post.id}
-                      title={post.title}
-                      body={post.body}
-                    />
-                  );
-                })}
-            </div>
-          );
-        })}
-      <div className="flex items-center justify-center">
-        <button
-          className="btn focus:text-slate-50 active:focus:text-slate-50"
-          onClick={() => {
-            fetchNextPage();
-          }}
-          disabled={isFetchingNextPage}
-        >
-          {isFetchingNextPage
-            ? "Loading more..."
-            : (data.pages.length ?? 0) < 25
-            ? "Load more"
-            : "Nothing more to load"}
-        </button>
+      <div className="w-[90%] lg:w-[70%] mx-auto flex flex-col lg:flex-row items-center justify-center lg:justify-around">
+        <Link href="/simple-loading" className="btn btn-wide btn-primary my-10">
+          Simple loading
+        </Link>
+        <Link href="/infinite-loading" className="btn btn-wide btn-accent">
+          Infinite loading
+        </Link>
       </div>
     </main>
   );
