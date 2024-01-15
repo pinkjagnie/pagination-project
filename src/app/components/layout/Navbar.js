ee82148ae948065cb665daf4354a1fe47fb24471
pinkjagnie <olga.krzysztofik@gmail.com>
2024-01-15 23:04:59 +0100

    Added simple navigation to simple & infinite loading pages

--- src/app/components/layout/Navbar.js
+++ src/app/components/layout/Navbar.js
@@ -1,8 +1,17 @@
+"use client";
+
 import Link from "next/link";
 
 import { FaRegNewspaper } from "react-icons/fa";
 
 const Navbar = () => {
+  const clickHandler = () => {
+    const elem = document.activeElement;
+    if (elem) {
+      elem?.blur();
+    }
+  };
+
   return (
     <>
       <div className="navbar fixed bg-base-300 z-50">
@@ -12,6 +21,20 @@ const Navbar = () => {
             <FaRegNewspaper size={30} />
             World News
           </Link>
+        </div>
+        <div className="flex-none">
+          <ul className="menu menu-horizontal px-1 font-medium">
+            <li onClick={clickHandler}>
+              <Link href="/simple-loading" className="focus:text-slate-50">
+                Simple
+              </Link>
+            </li>
+            <li onClick={clickHandler}>
+              <Link href="/infinite-loading" className="focus:text-slate-50">
+                Infinite
+              </Link>
+            </li>
+          </ul>
         </div>
       </div>
     </>
---
+++ src/app/simple-loading/page.js
@@ -0,0 +1,88 @@
+"use client";
+
+import { useEffect } from "react";
+
+import { useInfiniteQuery, useQueryClient } from "@tanstack/react-query";
+
+import ListOfPosts from "../components/ListOfPosts";
+import SinglePost from "../components/SinglePost";
+
+export default function Home() {
+  const queryClient = useQueryClient();
+
+  const getPosts = async (page = 1) => {
+    try {
+      const response = await fetch("/api/posts?page=" + page, {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+
+      const res = await response.json();
+      // console.log(res[3].id);
+      return res;
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
+
+  const { data, fetchNextPage, isFetchingNextPage } = useInfiniteQuery({
+    queryKey: ["query"],
+    queryFn: ({ pageParam }) => getPosts(pageParam),
+
+    getNextPageParam: (lastPage, pages) => {
+      return pages.length + 1;
+      // return lastPage.length > 0 ? pages.length + 1 : undefined;
+    },
+    initialData: { pages: [], pageParams: [1] },
+  });
+
+  useEffect(() => {
+    // Fetch initial data only once
+    if (data.pages.length === 0) {
+      getPosts(1);
+    }
+  }, []);
+
+  return (
+    <main className="min-h-screen bg-slate-50 pb-10">
+      <ListOfPosts />
+      {data.pages &&
+        data.pages.map((page, i) => {
+          return (
+            <div key={i}>
+              {page &&
+                page.map((post) => {
+                  return (
+                    <SinglePost
+                      key={post.id}
+                      title={post.title}
+                      body={post.body}
+                    />
+                  );
+                })}
+            </div>
+          );
+        })}
+      <div className="flex items-center justify-center">
+        <button
+          className="btn focus:text-slate-50 active:focus:text-slate-50"
+          onClick={() => {
+            fetchNextPage();
+          }}
+          disabled={isFetchingNextPage}
+        >
+          {isFetchingNextPage
+            ? "Loading more..."
+            : (data.pages.length ?? 0) < 25
+            ? "Load more"
+            : "Nothing more to load"}
+        </button>
+      </div>
+    </main>
+  );
+}
